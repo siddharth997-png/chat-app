cb87ecf120c8a7a7565af8e82f60fc29486b0914
Siddharth Kothari <siddharthkothari655@gmail.com>
2020-09-29 03:03:27 +0530

    added broadcast and send location featured

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,48 @@
-io()
+const socket = io()
+
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = document.querySelector('#inp')
+const $messageFormBtn = document.querySelector('#btn')
+const $sendLocBtn = document.querySelector('#send-location')
+
+socket.on('message',(message)=>{
+    console.log(message)
+})
+
+$messageForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    $messageFormBtn.setAttribute('disabled','disable')
+    const message = $messageFormInput.value
+
+    socket.emit('sendMessage',message, (error) => {
+
+        $messageFormBtn.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if(error){
+            return console.log(error)
+        }
+        console.log('Message delivered')
+    })
+})
+
+$sendLocBtn.addEventListener('click',() => {
+    if(!navigator.geolocation){
+        return alert('Your browser does not support geolocation')
+    }
+    $sendLocBtn.setAttribute('disabled','disabled')
+    navigator.geolocation.getCurrentPosition((position)=>{
+        console.log(position)
+        const pos = {
+            latitude : position.coords.latitude,
+            longitude : position.coords.longitude
+        }
+        socket.emit('sendLocation',pos,()=>{
+            $sendLocBtn.removeAttribute('disabled')
+            console.log('Location delivered')
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
-const express = require('express')
 const path = require('path')
 const http = require('http')
+const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { FILE } = require('dns')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirPath))
+app.use(express.static(publicDirectoryPath))
 
-io.on('connection',()=>{
-    console.log('New websocket Connection!')
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection finally')
+ 
+    socket.emit('message', 'Welcome new User')
+    socket.broadcast.emit('message','A new User has joined!')
+
+    socket.on('sendMessage',(message,cb)=>{
+
+        const filter = new Filter()
+        //io.emit('message',filter.clean(message))
+        if(!filter.clean(message)){
+            return cb('Bitch bad words are not allowed')
+        }
+
+        io.emit('message',message)
+        cb()
+    })
+
+    socket.on('sendLocation', ({latitude,longitude},cb)=>{
+        io.emit('message',`https://google.com/maps?q=${latitude},${longitude}`)
+        cb()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message','A user has disconnected!')
+    })
 })
 
-server.listen(port,()=>{
-    console.log('Server Running on port : ',port)
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
 })
